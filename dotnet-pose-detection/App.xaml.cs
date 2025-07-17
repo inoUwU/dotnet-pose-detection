d5f8206f51d5a7a5e763c4552a062e7a3cb678fc
ino <87593133+inoUwU@users.noreply.github.com>
2025-07-17 23:01:31 +0900

    プロジェクト ファイルを追加します。

---
+++ dotnet-pose-detection/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace dotnet_pose_detection
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ dotnet-pose-detection/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dotnet_pose_detection;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    string? _ImagePath = null;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OpenImageBtn_Click(object sender, RoutedEventArgs e)
+    {
+        var dlg = new Microsoft.Win32.OpenFileDialog
+        {
+            Title = "画像を選択してください。",
+            Filter = "画像ファイル|*.gif;*.png;*.jpeg;*.jpg;*.bmp|すべてのファイル|*.*"
+        };
+
+        if (dlg.ShowDialog() == true)
+        {
+            this._ImagePath = dlg.FileName;
+        }
+
+        this.OriginalImage.Source = this._ImagePath == null ? null : new BitmapImage(new Uri(this._ImagePath));
+    }
+
+    private void DetectByYoLoBtn_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void DetectByOpenCvBtn_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
